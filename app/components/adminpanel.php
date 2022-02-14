f20547bc4cfbb444241d0012a6c24328d6b5b00b
Your Andrej52 <andrejmrazik8@gmail.com>
2022-02-15 00:32:35 +0100

    adminpanel added

---
+++ app/components/adminpanel.php
@@ -0,0 +1,27 @@
+<ul>
+    <?php 
+    if (!isset($_SESSION['username'])) 
+        echo '  <a href="register">register</a>
+                <a href="login">login</a>      ';
+    else
+        {
+            echo '  <a href="../app/controllers/interactions.php?action=Logout">Logout</a>';
+            echo "  <div>prihlaseny ako: {$_SESSION['username']}</div>";
+            switch ($_SESSION['role']) {
+                case 'OPK-member':
+                    echo ' 
+                    <a href="add">add-topic</a>
+                    <a href="add2">add_gallery</a>
+                    <a href="select">select</a>
+                    ';
+                    break;
+                case 'user':
+                    echo ' <a href="select">select</a>';
+                    break;
+                default:
+                    break;
+            }
+        }
+       
+?>
+    </ul>
