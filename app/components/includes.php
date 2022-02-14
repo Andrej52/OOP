0af663f283337666cd53c1a8ffd6ae6f65b464c1
Your Andrej52 <andrejmrazik8@gmail.com>
2022-02-15 00:39:38 +0100

    updated handling

--- app/components/includes.php
+++
@@ -1,5 +0,0 @@
-<head>
-<link rel="stylesheet" href="main.css">
-<link rel="stylesheet" href="sidebar.css">
-<script defer src=""></script>
-</head>
--- app/init.php
+++ app/init.php
@@ -1,6 +1,5 @@
 <?php
 
 require  __DIR__."/core/app.php";
-require  __DIR__."/components/includes.php";
 require_once __DIR__."/models/database.php";
 require_once __DIR__."/models/topic.php";
