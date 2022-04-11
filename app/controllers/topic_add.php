6ff45bd3be479f9961f6e087326885515b45b094
Your Andrej52 <andrejmrazik8@gmail.com>
2022-04-12 00:21:49 +0200

    working uploading files and images by their's lenght

--- app/controllers/topic_add.php
+++ app/controllers/topic_add.php
@@ -17,19 +17,25 @@ $targetdir =$rootdir.$_POST['header']."-".date("Y");
         mkdir($targetdir."/doc");
     }
 }
-
-$topic->add($_POST);
+//var_dump($_FILES);
+//$topic->add($_POST);
 foreach ($_FILES as $key => $file)
 {
     for ($i=0; $i <sizeof($file['name']) ; $i++)
     { 
-        if ($file['name'] == "img") {
-            move_uploaded_file($file['tmp_name'][$i],$targetdir."/images/".$file['name'][$i]);
-            $topic->add($targetdir."/images/".$file['name'][$i]);
+        if (str_contains($file['type'][$i],"image")) {
+          echo "obrazok";
+          var_dump($file['name'][$i]);
+          move_uploaded_file($file['tmp_name'][$i],$targetdir."/images/".$file['name'][$i]);
+        
         }
         else
         {
+            echo "dokument";
+            var_dump($file['name'][$i]);
             move_uploaded_file($file['tmp_name'][$i],$targetdir."/doc/".$file['name'][$i]);
+           // $topic->add($targetdir."/images/".$file['name'][$i]);
+           
         }
     
     }
